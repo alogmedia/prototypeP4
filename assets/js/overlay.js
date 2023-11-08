a7c24b762210c05c4bea3464597e07758632b68f
alogmedia <albertlogn@gmail.com>
2023-11-08 15:29:44 +0100

    overlay og lidt af hvert

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -1,6 +1,9 @@
 // Get both modal elements and the CTA button elements
 const cityModelOverlay = document.getElementById('byModel');
 const infoModelOverlay = document.getElementById('infoModel');
+const betalingModelOverlay = document.getElementById('betalingModel');
+const afslutModelOverlay = document.getElementById('afslutModel');
+
 const ctaKnapper = document.querySelectorAll('.cta-knap');
 
 // Open the byModel when any "Lej Stand" button is clicked
@@ -10,16 +13,26 @@ ctaKnapper.forEach(btn => {
         if (btn.getAttribute("data-action") === "skift-til-infoModel") {
             cityModelOverlay.style.display = 'none'; // Hide the first modal
             infoModelOverlay.style.display = 'flex'; // Display the second modal
+        } else if (btn.getAttribute("data-action") === "skift-til-betalingModel") {
+            infoModelOverlay.style.display = 'none'; // Display the second modal
+            betalingModelOverlay.style.display = 'flex'; // Hide the first modal
+        } else if (btn.getAttribute("data-action") === "skift-til-afslutModel") {
+            betalingModelOverlay.style.display = 'none'; // Display the second modal
+            afslutModelOverlay.style.display = 'flex'; // Hide the first modal
         } else if (btn.getAttribute("data-action") === "OpenByModel") {
             cityModelOverlay.style.display = 'flex'; // Display the first modal
+        } else if (btn.getAttribute("data-action") === "afslut") {
+            afslutModelOverlay.style.display = 'none'; // Display the first modal
         }
     });
 });
 
 // Close the modals when clicking outside the modal content
 window.addEventListener('click', function(event) {
-    if (event.target === cityModelOverlay || event.target === infoModelOverlay) {
+    if (event.target === cityModelOverlay || event.target === infoModelOverlay || event.target === betalingModelOverlay || event.target === afslutModelOverlay) {
         cityModelOverlay.style.display = 'none';
         infoModelOverlay.style.display = 'none';
+        betalingModelOverlay.style.display = 'none';
+        afslutModelOverlay.style.display = 'none';   
     }
 });
