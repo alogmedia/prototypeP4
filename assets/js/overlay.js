3f7a24d201e146c15d8841be31b8f9408d9a8776
alogmedia <albertlogn@gmail.com>
2023-11-08 10:35:50 +0100

    Fixed overlay

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -1,27 +1,25 @@
 // Get both modal elements and the CTA button elements
-const cityModel = document.getElementById('byModel');
-const infoModel = document.getElementById('infoModel');
+const cityModelOverlay = document.getElementById('byModel');
+const infoModelOverlay = document.getElementById('infoModel');
 const ctaKnapper = document.querySelectorAll('.cta-knap');
 
 // Open the byModel when any "Lej Stand" button is clicked
 ctaKnapper.forEach(btn => {
     btn.addEventListener('click', function(event) {
-        // Prevent event propagation to ensure the window click event doesn't interfere
-        event.stopPropagation();
+        event.stopPropagation(); // Stop propagation to ensure the window click event doesn't interfere
         if (btn.getAttribute("data-action") === "skift-til-infoModel") {
-            // Switch to the second modal when "Videre" is clicked
-            byModel.style.display = 'none';
-            infoModel.style.display = 'flex';
+            cityModelOverlay.style.display = 'none'; // Hide the first modal
+            infoModelOverlay.style.display = 'flex'; // Display the second modal
         } else if (btn.getAttribute("data-action") === "OpenByModel") {
-            byModel.style.display = 'flex';
+            cityModelOverlay.style.display = 'flex'; // Display the first modal
         }
     });
 });
 
 // Close the modals when clicking outside the modal content
 window.addEventListener('click', function(event) {
-    if (event.target === byModel || event.target === infoModel) {
-        byModel.style.display = 'none';
-        infoModel.style.display = 'none';
+    if (event.target === cityModelOverlay || event.target === infoModelOverlay) {
+        cityModelOverlay.style.display = 'none';
+        infoModelOverlay.style.display = 'none';
     }
 });
