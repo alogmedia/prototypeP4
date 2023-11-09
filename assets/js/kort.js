941d69ba49f3b0239eaddce9f14f1f09db4a9ea2
Simonhkristiansen <simonhkristiansen@gmail.com>
2023-11-09 09:44:01 +0100

    haløjsovs

--- assets/js/kort.js
+++ assets/js/kort.js
@@ -1,23 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var pinpointsInfo = {
-        'pinpoint-1': '<h3>Odense Havn</h3><p class=adressetxt>EDIONSVEJ 5000 ODENSE</p><p class=tidtxt>20. maj 09:00-17:00</p>',
-        'pinpoint-2': 'Copenhagen, the capital of Denmark...'
+    let nålsInfo = {
+        'nål-1': '<h3>Odense Havn</h3><p class=adressetxt>EDIONSVEJ 5000 ODENSE C</p><p class=tidtxt>20. maj 09:00-17:00</p>',
+        'nål-2': '<h3>København Rådhusplads</h3><p class=adressetxt>1599 KØBENHAVN V</p><p class=tidtxt>20. maj 09:00-17:00</p>',
+        'nål-3': '<h3>Ålborg Havn</h3><p class=adressetxt>EDIONSVEJ POUL BUÅS VEJ 34, 9000 AALBORG</p><p class=tidtxt>20. maj 09:00-17:00</p>',
+        'nål-4': '<h3>Århus</h3><p class=adressetxt>INGERSLEVS BOULEletD, 8000 AARHUS C</p><p class=tidtxt>20. maj 09:00-17:00</p>',
+        'nål-5': '<h3>Kolding</h3><p class=adressetxt>AMBOLTEN 1-6, 6000 KOLDING</p><p class=tidtxt>20. maj 09:00-17:00</p>',
+        'nål-6': '<h3>Under Broen</h3><p class=adressetxt>GL. STRANDVEJ, 5500 MIDDELFART </p><p class=tidtxt>20. maj 09:00-17:00</p>',
+        'nål-7': '<h3>Roskilde</h3><p class=adressetxt>MØLLEHUSVEJ 15, 4000 ROSKILDE</p><p class=tidtxt>20. maj 09:00-17:00</p>',
+        'nål-8': '<h3>Nyborg Rådhus</h3><p class=adressetxt>TORVET 1, 5800 NYBORG</p><p class=tidtxt>20. maj 09:00-17:00</p>',
+        'nål-9': '<h3>Vejle Rådhus</h3><p class=adressetxt>RÅDHUSTORVET 1, 7100 VEJLE </p><p class=tidtxt>20. maj 09:00-17:00</p>',
     };
 
-    var pinpoints = document.getElementsByClassName('pinpoint');
-    var overlay = document.getElementById('overlayx');
-    var overlayText = document.getElementById('overlayx-text');
+    let nåls = document.getElementsByClassName('nål');
+    let overlay = document.getElementById('overlayx');
+    let overlayText = document.getElementById('overlayx-text');
 
     function showOverlay(event) {
-        // Ensures we get the pinpoint element even if the target is a child
-        var pinpoint = event.currentTarget; 
-        var info = pinpointsInfo[pinpoint.id];
+        let nål = event.currentTarget; 
+        let info = nålsInfo[nål.id];
         overlayText.innerHTML = info;
 
-        // Positioning logic adjusted to make sure the overlay appears next to the pinpoint
-        var rect = pinpoint.getBoundingClientRect();
+        let rect = nål.getBoundingClientRect();
         overlay.style.top = (rect.top + window.scrollY - overlay.offsetHeight - 10) + 'px';
-        overlay.style.left = (rect.left + window.scrollX - overlay.offsetWidth / 2 + pinpoint.offsetWidth / 2) + 'px';
+        overlay.style.left = (rect.left + window.scrollX - overlay.offsetWidth / 2 + nål.offsetWidth / 2) + 'px';
         overlay.style.display = 'block';
     }
 
@@ -25,8 +30,8 @@ document.addEventListener('DOMContentLoaded', function () {
         overlay.style.display = 'none';
     }
 
-    Array.from(pinpoints).forEach(function (pinpoint) {
-        pinpoint.addEventListener('mouseenter', showOverlay); // Use mouseenter instead of mouseover
-        pinpoint.addEventListener('mouseleave', hideOverlay); // Use mouseleave instead of mouseout
+    Array.from(nåls).forEach(function (nål) {
+        nål.addEventListener('mouseenter', showOverlay);
+        nål.addEventListener('mouseleave', hideOverlay); 
     });
 });
