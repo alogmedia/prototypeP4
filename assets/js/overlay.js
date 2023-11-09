105281d931d2ab93571d47306300120e25e20d5e
alogmedia <albertlogn@gmail.com>
2023-11-09 08:46:12 +0100

    Kommentare til JS

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -1,4 +1,4 @@
-// Get both modal elements and the CTA button elements
+// Hent model elementer og CTA knapperne
 const cityModelOverlay = document.getElementById('byModel');
 const infoModelOverlay = document.getElementById('infoModel');
 const betalingModelOverlay = document.getElementById('betalingModel');
@@ -6,37 +6,37 @@ const afslutModelOverlay = document.getElementById('afslutModel');
 
 const ctaKnapper = document.querySelectorAll('.cta-knap');
 
-// Open the byModel when any "Lej Stand" button is clicked
+// Åben vælg by overlay når en "Lej Stand" CTA knap bliver klikket på
 ctaKnapper.forEach(btn => {
     btn.addEventListener('click', function(event) {
         event.stopPropagation(); // Stop propagation to ensure the window click event doesn't interfere
         if (btn.getAttribute("data-action") === "skift-til-infoModel") {
-            cityModelOverlay.style.display = 'none'; // Hide the first modal
-            infoModelOverlay.style.display = 'flex'; // Display the second modal
+            cityModelOverlay.style.display = 'none'; // Display none(Gem den væk)
+            infoModelOverlay.style.display = 'flex'; // Vis det næste overlay
         } else if (btn.getAttribute("data-action") === "skift-til-betalingModel") {
-            infoModelOverlay.style.display = 'none'; // Display the second modal
-            betalingModelOverlay.style.display = 'flex'; // Hide the first modal
+            infoModelOverlay.style.display = 'none'; 
+            betalingModelOverlay.style.display = 'flex';
         } else if (btn.getAttribute("data-action") === "skift-til-afslutModel") {
-            betalingModelOverlay.style.display = 'none'; // Display the second modal
-            afslutModelOverlay.style.display = 'flex'; // Hide the first modal
+            betalingModelOverlay.style.display = 'none'; 
+            afslutModelOverlay.style.display = 'flex'; 
         } else if (btn.getAttribute("data-action") === "OpenByModel") {
-            cityModelOverlay.style.display = 'flex'; // Display the first modal
+            cityModelOverlay.style.display = 'flex'; 
         } else if (btn.getAttribute("data-action") === "afslut") {
-            afslutModelOverlay.style.display = 'none'; // Display the first modal
+            afslutModelOverlay.style.display = 'none';
         }
     });
 });
 
-// Get the image element by its ID
+// Find mobilepay billedet på ID
 const mobilepayImage = document.getElementById('mobilepayImage');
 
-// Bind a click event to the image
+// Tildel klik event til billedet
 mobilepayImage.addEventListener('click', function() {
-    // Perform the same action as the button with data-action="skift-til-afslutModel"
-    betalingModelOverlay.style.display = 'none'; // Hide the current modal
-    afslutModelOverlay.style.display = 'flex';   // Display the next modal
+    // Gør det samme som når man trykker betal
+    betalingModelOverlay.style.display = 'none';
+    afslutModelOverlay.style.display = 'flex';
 });
-// Close the modals when clicking outside the modal content
+// Klikker man udenfor overlay skal den lukke formularen.
 window.addEventListener('click', function(event) {
     if (event.target === cityModelOverlay || event.target === infoModelOverlay || event.target === betalingModelOverlay || event.target === afslutModelOverlay) {
         cityModelOverlay.style.display = 'none';
